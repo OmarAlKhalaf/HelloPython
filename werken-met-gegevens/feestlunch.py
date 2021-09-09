58e58658aa2e441c0aa5f7a4e8e75b923d064bd8
OmarAlKhalaf <99047548@mydavinci.nl>
2021-09-09 13:41:56 +0200

    ther is 2 new photo and 2 new codingfolder's

---
+++ werken-met-gegevens/feestlunch.py
@@ -0,0 +1,9 @@
+croissantjes=17
+prijsvoorndecroissantjes=0.39
+stokbroden=2
+prijsvoorstokbroden=2.78
+kortingsbonnen=3
+prijsinkortingsbonnen=0.50
+
+print(prijsvoorndecroissantjes*croissantjes+prijsvoorstokbroden*stokbroden-prijsinkortingsbonnen*kortingsbonnen)
+print("De feestlunch kost je bij de bakker 18.88 euro voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!")
---
+++ werken-met-gegevens/speelhaldag.py
@@ -0,0 +1,8 @@
+Persoon=4
+toegangsticket=7.45
+minuteninspelen=45
+kostperpersoon=0.37
+inminuten=5
+
+print(toegangsticket*Persoon+kostperpersoon*minuteninspelen)
+print("Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar 44.44 euro")
