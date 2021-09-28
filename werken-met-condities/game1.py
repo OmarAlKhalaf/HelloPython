b9b605fee45fdda5dcc2eac9858a0e4bf217871d
OmarAlKhalaf <99047548@mydavinci.nl>
2021-09-28 11:42:51 +0200

    refrsh for game.py

--- werken-met-condities/game1.py
+++ werken-met-condities/game1.py
@@ -45,15 +45,24 @@ Sword with shield, Longsword, Bow and arrow, Dual short swords.
 2: Longsword
 3: Bow and arrow
 4: Dual swords
-Wat do u choose? 1/2/3/4 : """ + bcolors.ENDC)
-            if start4 == "1":
+Wat do u choose? type the weapon name : """ + bcolors.ENDC)
+            if start4 == "1" or start4 == "Sword and shield":
                 print(bcolors.OKBLUE + "You select Sword and shield"+ bcolors.ENDC)
-            elif start4 == "2":
+            elif start4 == "2" or start4 == "Longsword":
                 print(bcolors.OKBLUE +"You select Longsword"+ bcolors.ENDC)
-            elif start4 == "3":
+            elif start4 == "3" or start4 == "Bow and arrow":
                 print(bcolors.OKBLUE +"You select Bow and arrow"+ bcolors.ENDC)
-            elif start4 == "4":
+            elif start4 == "4" or start4 == "Dual swords":
                 print(bcolors.OKBLUE +"You select Dual swords"+ bcolors.ENDC)
-            print(bcolors.OKBLUE +"aaaaa "+ name1 + bcolors.ENDC)
+            print(bcolors.OKBLUE +"Someone walk in to u,Hi are u new here ? i heard that your name is "+ name1 + "?" + bcolors.ENDC)
+            start5 = input(bcolors.UNDERLINE + bcolors.HEADER + "is that correct ?" + bcolors.ENDC)
+            print(bcolors.OKBLUE +"Great to see u " + name1 + " i am general kapa, i am here to say i am glad that we have new solder" + bcolors.ENDC)
+            print(bcolors.OKBLUE + "we did not get a new solder in year's now and we need them so badly,now be ready u com in a realy good time tomorrow we will go Release the land from the Goblens."+ bcolors.ENDC)
+            print(bcolors.OKBLUE + "is the new day and you are in the field you got your "+ start4 + " and you are ready to fight."+ bcolors.ENDC)
+            print(bcolors.OKBLUE + "you was with your team but the ground break and you are alone now, you are walking and you see 2 ways"+ bcolors.ENDC)
+            print(bcolors.OKBLUE +"""Way 1 : long but not really safe 
+Way 2 : short but really dangerous"""+ bcolors.ENDC)
+            start6 = input(bcolors.UNDERLINE + bcolors.HEADER + "Which way do u wanna go with? 1/2 : " + bcolors.ENDC)
+
 else:
     print(bcolors.OKCYAN + "well it's up to " + name + " you see you later " + bcolors.ENDC)
