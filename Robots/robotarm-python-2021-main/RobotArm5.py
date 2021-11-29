9bfade7beb620cd14b763f1702d596927badf83a
OmarAlKhalaf <99047548@mydavinci.nl>
2021-11-29 16:06:35 +0100

    (robotarm6 done)

--- Robots/robotarm-python-2021-main/RobotArm5.py
+++ Robots/robotarm-python-2021-main/RobotArm5.py
@@ -3,6 +3,7 @@ from RobotArm import RobotArm
 robotArm = RobotArm('exercise 6')
 
 # Jouw python instructies zet je vanaf hier:
+robotArm.speed=5
 for x in range (1,8):
     robotArm.moveRight()
 robotArm.grab()
@@ -15,7 +16,3 @@ for q in range (1,8):
     robotArm.grab()
     robotArm.moveRight()
     robotArm.drop()
-
-
-# Na jouw code wachten tot het sluiten van de window:
-robotArm.wait()
--- Robots/robotarm-python-2021-main/RobotArm6.py
+++ Robots/robotarm-python-2021-main/RobotArm6.py
@@ -3,7 +3,15 @@ from RobotArm import RobotArm
 robotArm = RobotArm('exercise 7')
 
 # Jouw python instructies zet je vanaf hier:
-
-
+robotArm.speed=4
+for e in range (1,6):
+    for q in range (1,7):
+    
+        robotArm.moveRight();   
+        robotArm.grab()
+        robotArm.moveLeft()
+        robotArm.drop()
+    for w in range (1,3):
+        robotArm.moveRight()
 # Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()
