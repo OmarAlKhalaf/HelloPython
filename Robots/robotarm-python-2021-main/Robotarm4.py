fb14146a5d1c3042281a91fc2fd3a9b3fca168eb
OmarAlKhalaf <99047548@mydavinci.nl>
2021-11-29 13:43:27 +0100

    (i end the robotarm4 project)

--- Robots/robotarm-python-2021-main/Robotarm4.py
+++ Robots/robotarm-python-2021-main/Robotarm4.py
@@ -3,19 +3,35 @@ from RobotArm import RobotArm
 robotArm = RobotArm('exercise 4')
 
 # Jouw python instructies zet je vanaf hier:
-for q in range(1,4,):
+for q in range(1,4):
     for w in range(1,3):
         robotArm.grab();
-        robotArm.moveRight();
-    robotArm.drop();
+        robotArm.moveRight()
+    robotArm.drop()
     for e in range(1,3):
-        robotArm.moveLeft();
-for a in range(1,3):
-    robotArm.moveRight();
-    robotArm.grab()
-    for s in range(2,3):
-        robotArm.moveLeft();
-        robotArm.drop()  
+        robotArm.moveLeft()
+for k in range(1):
+    for i in range(2):
+        robotArm.moveRight()
+    robotArm.grab();
+    for o in range(1):
+        robotArm.moveLeft()
+    robotArm.drop()
+for k in range(1,3):
+    for i in range(1):
+        robotArm.moveRight()
+    robotArm.grab();
+    for o in range(1):
+        robotArm.moveLeft()
+    robotArm.drop()
+    
 
 # Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()
+    
+    #for r in range (1,3):
+     #   for m in range (1,2):
+    #        robotArm.moveRight();
+   #     robotArm.grab()
+  #      robotArm.moveLeft()
+ #       robotArm.drop()
