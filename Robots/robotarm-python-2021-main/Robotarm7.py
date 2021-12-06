36437a353c0d92eaed5385637df75f7dbc0a5e79
OmarAlKhalaf <99047548@mydavinci.nl>
2021-12-06 12:15:45 +0100

    (project 7 done)

--- Robots/robotarm-python-2021-main/Robotarm7.py
+++ Robots/robotarm-python-2021-main/Robotarm7.py
@@ -1,1 +1,18 @@
-a = (help)
+from RobotArm import RobotArm
+
+robotArm = RobotArm('exercise 8')
+
+# Jouw python instructies zet je vanaf hier:
+robotArm.speed=5
+for q in range(1,2):
+    for e in range(1,8):
+        robotArm.moveRight()
+        robotArm.grab()
+        for w in range(1,9):
+            robotArm.moveRight()
+        robotArm.drop()
+        for r in range(1,10):
+            robotArm.moveLeft()
+
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
